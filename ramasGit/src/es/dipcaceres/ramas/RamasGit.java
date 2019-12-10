5d1a1aa636d2e4c58d303b2e39a1cbde233859e7
moai <moai_69@yahoo.es>
2019-12-10 09:50:35 +0100

    otras ramas

--- ramasGit/src/es/dipcaceres/ramas/RamasGit.java
+++ ramasGit/src/es/dipcaceres/ramas/RamasGit.java
@@ -2,8 +2,8 @@ package es.dipcaceres.ramas;
 
 public class RamasGit {
 	public static void main(String[] args) {
-		System.out.println("Esto son ramas");
-		System.out.println("Esto son ramas");
-		System.out.println("Esto son ramas"); 
+		System.out.println("Esto son otras ramas");
+		System.out.println("Esto son otras ramas");
+		System.out.println("Esto son otras ramas"); 
 	}
 }
