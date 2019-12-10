3eb16ab6571db15cd4454f84fad8200bd77f22d3
moai <moai_69@yahoo.es>
2019-12-10 09:27:15 +0100

    aa

--- ramasGit/src/es/dipcaceres/ramas/RamasGit.java
+++ ramasGit/src/es/dipcaceres/ramas/RamasGit.java
@@ -4,6 +4,6 @@ public class RamasGit {
 	public static void main(String[] args) {
 		System.out.println("Esto son ramas");
 		System.out.println("Esto son ramas");
-		System.out.println("Esto son ramas");
+		System.out.println("Esto son ramas"); 
 	}
 }
